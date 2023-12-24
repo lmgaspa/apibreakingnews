a3961198ac8f88ee04329aa548c1abdda0a1f5b6
Luiz <seu-email@example.com>
2023-12-23 22:33:39 -0300

    final

--- index.js
+++ index.js
@@ -11,17 +11,12 @@ const cors = require('cors');
 
 dotenv.config();
 
-const allowedOrigins = ['http://localhost:3000', 'https://api-breakingnews-jfhc.onrender.com', 'https://api-breakingnews.onrender.com', 'https://api-breakingnews.onrender.com/user/create.'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+cors.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 const port = process.env.PORT || 3000;
 
