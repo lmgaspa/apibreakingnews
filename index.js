98510c66a808da407935b841b3988fd8c4abbc09
Luiz <seu-email@example.com>
2023-12-24 08:56:10 -0300

    finaal

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ const cors = require('cors');
 
 dotenv.config();
 
-cors.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
